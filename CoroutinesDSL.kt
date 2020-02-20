2131cd6eed7fd35c85f8ba648f0c428e06954be7
Slavik Urdzik <Urdziksv@gmail.com>
2020-02-20 13:21:57 +0200

    Commit

--- CoroutinesDSL.kt
+++ CoroutinesDSL.kt
@@ -1,1 +1,32 @@
+fun startJob(
+        parentScope: CoroutineScope,
+        coroutineContext: CoroutineContext,
+        block: suspend CoroutineScope.() -> Unit
+) {
+    parentScope.launch(coroutineContext) {
+        supervisorScope {
+            block()
+        }
+    }
+}
 
+suspend fun <T> startTask(
+        coroutineContext: CoroutineContext,
+        block: suspend CoroutineScope.() -> T
+): T {
+    return withContext(coroutineContext) {
+        return@withContext block()
+    }
+}
+
+fun <T> startTaskAsync(
+        parentScope: CoroutineScope,
+        coroutineContext: CoroutineContext,
+        block: suspend CoroutineScope.() -> T
+): Deferred<T> {
+    return parentScope.async(coroutineContext) {
+        return@async supervisorScope {
+            return@supervisorScope block()
+        }
+    }
+}
